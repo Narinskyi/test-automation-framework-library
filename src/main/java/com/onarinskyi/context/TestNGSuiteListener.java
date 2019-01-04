aac701ca0fc5db4f1d6ad90eafacad7162d0a25e
onarinskyi <onarinskyi@griddynamics.com>
2019-01-04 16:38:11 +0100

    Update maven jar plugin version

--- src/main/java/com/onarinskyi/context/TestNGSuiteListener.java
+++ src/main/java/com/onarinskyi/context/TestNGSuiteListener.java
@@ -1,11 +1,14 @@
 package com.onarinskyi.context;
 
 import com.onarinskyi.utils.Report;
-import org.apache.log4j.Logger;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
 
 public class TestNGSuiteListener implements ISuiteListener {
+    @Override
+    public void onStart(ISuite iSuite) {
+
+    }
 
     @Override
     public void onFinish(ISuite iSuite) {
